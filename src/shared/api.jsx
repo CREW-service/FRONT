e75b86d590416c3f6b950b56c522b40b3cdbdffb
Hira-Yuki <cjsdlf44@gmail.com>
2023-06-17 17:23:07 +0900

    Api 파일 수정

--- src/shared/api.jsx
+++ src/shared/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // 싱글톤 패턴으로 axios 인스터스를 생성
-const instance = axios.create({
+const api = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_SERVER_URL,
   headers: {
     withCredentials: true,
@@ -12,9 +12,3 @@ export const AuthApi = {
   imgUpload: (payload) => api.post('/job/upload', payload),
   write: (payload, config) => api.post('/job/write', payload, { ...config }),
 };
-
-export const KakaologinAPI = {
-  getLogin: () => instance.get('/auth/kakao'),
-};
-
-export default instance;
