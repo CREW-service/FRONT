d057f24376b3378b1f2e14b98356a9cd02152805
coderia <113007887+coderia8984@users.noreply.github.com>
2023-06-23 01:47:01 +0900

    detail : Captainpage Crewpage 생성

--- src/Components/feature/Detail/Detail.jsx
+++ src/Components/feature/Detail/Detail.jsx
@@ -3,26 +3,12 @@ import { useParams } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import DOMPurify from "dompurify"; // DOMPurify 라이브러리 가져오기
 import AuthApi from "shared/api";
+import Captindetail from "./Captindetail";
+import Crewdetail from "./Crewdetail";
+import Otherpeople from "./Otherpeople";
 // import Detailoptinmodal from "./Detailoptinmodal";
 
 function Detail() {
-  const { id } = useParams();
-  const [boat, setBoat] = useState("");
-
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchBoat = async () => {
-    try {
-      const { data } = await AuthApi.getBoatDetail(id);
-      setBoat(data.boat);
-      console.log(data);
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Error fetching post:", error);
-      setIsLoading(false);
-    }
-  };
-
   const [cookies] = useCookies(["authorization"]);
   const config = {
     headers: {
@@ -31,89 +17,107 @@ function Detail() {
     },
   };
 
-  const joinBoatHandler = async () => {
+  const { id } = useParams();
+
+  const [boat, setBoat] = useState("");
+  const [personType, setPersonType] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchBoat = async () => {
     try {
-      const res = await AuthApi.joinBoat(id, config);
-      {
-        console.log(res);
-      }
-      // console.log(res);
+      const { data } = await AuthApi.getBoatDetail(id, config);
+      // console.log("data", data);
+      setBoat(data.boat);
+      setPersonType(data.personType);
+      console.log("data", data);
+      setIsLoading(false);
     } catch (error) {
-      console.log("Error:", error);
-      alert(error.response.data.errorMessage);
+      console.error("Error fetching post:", error);
+      setIsLoading(false);
     }
   };
-
-  // 버튼 클릭시 참여하기 버튼 안보이기
-  const [isVisible, setIsVisible] = useState(true);
-  const visibleHandler = () => {
-    setIsVisible(false);
-  };
-  // 버튼 클릭시 댓글 div 보이기
-  const [showComment, setShowComment] = useState(false);
-  const ShowCommentClickBtn = () => {
-    setShowComment(true);
-  };
-
-  // // 모달창 노출 여부 state
-  // const [modalOpen, setModalOpen] = useState(false);
-
-  // // 모달창 노출
-  // const showModal = () => {
-  //   setModalOpen(true);
-  // };
 
   useEffect(() => {
     fetchBoat();
   }, []);
 
-  return (
-    <div>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <div>제목 : {boat.title}</div>
-          <div>
-            작성일 : {new Date(boat.createdAt).toISOString().split("T")[0]}
-            <br />
-            작성자 : {boat.captain}
-          </div>
-          <div>지역: {boat.address}</div>
-          <div>모집 기한: {boat.endDate}</div>
-          <div>
-            모집 인원: {boat.crewNum}/{boat.maxCrewNum}
-          </div>
-          <div>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: DOMPurify.sanitize(boat.content),
-              }}
-            />
-          </div>
-          {isVisible && (
-            <button
-              type="button"
-              onClick={() => {
-                joinBoatHandler();
-                visibleHandler();
-                ShowCommentClickBtn();
-              }}
-            >
-              참여하기
-            </button>
-          )}
-          {showComment && <div>comment</div>}
+  const joinBoatHandler = async () => {
+    try {
+      const res = await AuthApi.joinBoat(id, config);
+      {
+        console.log("res", res);
+      }
+    } catch (error) {
+      console.log("Error:", error);
+      alert("참가못했습니다");
+      // alert(error.response.data.errorMessage);
+    }
+  };
 
-          {/* <div>
-            <button type="button" onClick={showModal}>
-              모달띄우기
-            </button>
-            {modalOpen && <Detailoptinmodal setModalOpen={setModalOpen} />}
-          </div> */}
-        </>
-      )}
-    </div>
+  // 댓글 창 구현
+  const [comments, setComments] = useState("");
+  const commentChangeHandler = (event) => {
+    setComments(event.target.value);
+  };
+  // 댓글 게시 버튼 클릭
+  const clickAddBtnHandler = () => {
+    const newComment = {
+      commentId: comments.length + 1,
+    };
+    setComments([...comments, newComment]);
+    setComments("");
+  };
+
+  let componentToRender;
+
+  if (isLoading) {
+    componentToRender = <div>Loading...</div>;
+  } else if (personType === "captain") {
+    componentToRender = <Captindetail boat={boat} />;
+  } else if (personType === "crew") {
+    componentToRender = (
+      <Crewdetail
+        boat={boat}
+        comments={comments}
+        commentChangeHandler={commentChangeHandler}
+        clickAddBtnHandler={clickAddBtnHandler}
+      />
+    );
+  } else {
+    componentToRender = <Otherpeople joinBoatHandler={joinBoatHandler} />;
+  }
+
+  return (
+    <div>{componentToRender}</div>
+
+    //
+
+    //       <button
+    //         type="button"
+    //         onClick={() => {
+    //           joinBoatHandler();
+    //         }}
+    //       >
+    //         참여하기
+    //       </button>
+
+    //       <div>
+    //         <div>
+    //           <input
+    //             type="text"
+    //             placeholder="댓글을 입력하세요"
+    //             value={comments}
+    //             onChange={commentChangeHandler}
+    //           />
+    //           <button type="button" onClick={clickAddBtnHandler}>
+    //             게시
+    //           </button>
+    //         </div>
+    //         <div>{comments}</div>
+    //       </div>
+    //     </>
+    //   )}
+    // </div>
   );
 }
 
