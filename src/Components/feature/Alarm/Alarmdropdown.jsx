56aabc420c9454eccfa3fbb845e88f90528b7917
Hira-Yuki <cjsdlf44@gmail.com>
2023-06-25 23:48:38 +0900

    지도 위로 모달이 띄워지도록 수정

--- src/Components/feature/Alarm/Alarmdropdown.jsx
+++ src/Components/feature/Alarm/Alarmdropdown.jsx
@@ -112,6 +112,7 @@ const modalStyles = {
     right: 0,
     bottom: 0,
     backgroundColor: "rgba(0, 0, 0, 0)",
+    zIndex: 1000, // Added z-index property
   },
   content: {
     position: "absolute",
