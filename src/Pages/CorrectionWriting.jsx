7298833d0a764c349a512750a90e1ffcf905e349
coderia <113007887+coderia8984@users.noreply.github.com>
2023-06-25 21:33:41 +0900

    correction

---
+++ src/Pages/CorrectionWriting.jsx
@@ -0,0 +1,13 @@
+import Layout from "Components/Layout/Layout";
+import Correctioneditor from "Components/feature/Editor/Correctioneditor";
+import React from "react";
+
+function CorrectionWriting() {
+  return (
+    <Layout>
+      <Correctioneditor />
+    </Layout>
+  );
+}
+
+export default CorrectionWriting;
